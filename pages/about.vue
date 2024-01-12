aadd7473b4285266f127e6a262eb58c5d3273ee2
unknown <you@example.com>
2024-01-12 14:38:21 +0000

    updated images

--- pages/about.vue
+++ pages/about.vue
@@ -17,12 +17,12 @@
               <p class="text-gray-600 mb-8">Their primary target will be the power generators. Prepare to open the shield. Sir, Rebel ships are coming into our sector. Good. Our first catch of the day. Stand by, ion control....Fire! The first transport is away.</p>
             </div>
             <div class="w-full md:w-1/2">
-              <img class="rounded-lg" src="https://pbs.twimg.com/media/CR45LOXVEAADG5E.jpg" alt="Vortex" />
+              <img class="rounded-lg" src="../public/images/arturo-rey-5yP83RhaFGA-unsplash.jpg" alt="Vortex" />
             </div>
           </div>
           <div class="flex items-center flex-wrap mb-20">
             <div class="w-full md:w-1/2">
-              <img class="rounded-lg" src="https://www.thesun.co.uk/wp-content/uploads/2019/06/SWJFO-EAPlay-08-1.jpg" alt="use the force" />
+              <img class="rounded-lg" src="../public/images/burgess-milner-OYYE4g-I5ZQ-unsplash.jpg" alt="use the force" />
             </div>
             <div class="w-full md:w-1/2 pl-10">
               <h4 class="text-3xl text-gray-800 font-bold mb-3">Use the Force!</h4>
@@ -35,7 +35,7 @@
               <p class="text-gray-600 mb-8">There is no try. I can't. It's too big. Size matters not. Look at me. Judge me by my size, do you? Hm? Mmmm. And well you should not. For my ally in the Force. And a powerful ally it is.</p>
             </div>
             <div class="w-full md:w-1/2">
-              <img class="rounded-lg" src="https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Ferikkain%2Ffiles%2F2018%2F01%2FRey-Luke.jpg" alt="Syncing" />
+              <img class="rounded-lg" src="../public/images/suheyl-burak-p7I07kuPSyU-unsplash.jpg" alt="Syncing" />
             </div>
           </div>
         </section>
